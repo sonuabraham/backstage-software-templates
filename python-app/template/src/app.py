05193307903b72a212c5f733a1962459ee31d501
Sonu Abraham <sonuabraham2001@gmail.com>
2026-04-18 12:19:39 +1000

    Add python app software template.

---
+++ python-app/template/src/app.py
@@ -0,0 +1,29 @@
+
+'/aop/v1/healthz'
+
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        'hostname': socket.gethostname(),
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on  %B %d , %Y"),
+        'message' : "Hello from Sonu!!!!!!!!!!)",
+        'deployed_on' : "kubernetes",
+        'env' : '${{values.app_env}}',
+        'app_name' : '${{values.app_env}}'
+    }
+    )
+@app.route('/api/v1/healthz')
+def health():
+     return jsonify({
+        'status': "up"
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
